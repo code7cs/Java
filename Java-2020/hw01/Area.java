2f4e1bab30d84576c50e6b82c07ffbaa84aeac1b
hanfanw <hanfanw@gmail.com>
2020-05-19 12:55:48 -0400

    update

---
+++ Java-2020/hw01/Area.java
@@ -0,0 +1,11 @@
+package hw01;
+
+public class Area {
+    public static void printArea(int x , int y){
+        System.out.println(x * y);
+    }
+
+    public static void printArea(int x ) {
+        System.out.println( x * x);
+    }
+}
