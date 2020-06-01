f785a49fbe48fa4547d88bfcde623af2cfd232ec
hanfanw <hanfanw@gmail.com>
2020-05-31 21:47:07 -0400

    initial spring projecct

--- src/com/antra/college/College.java
+++ src/com/antra/college/College.java
@@ -1,4 +1,9 @@
 package com.antra.college;
 
+import java.sql.SQLOutput;
+
 public class College {
+    public static void main(String[] args) {
+        System.out.println("hello wrold");
+    }
 }
